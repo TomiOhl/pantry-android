6c70fee46c5db4a2b5db2e39867daa8003091f1b
Tomi_Ohl <ohlslager.tom@gmail.com>
2021-02-15 18:30:50 +0100

    Család bekérése indításkor, ha még nem volt megadva

--- app/src/main/java/com/tomi/ohl/szakdoga/SplashActivity.java
+++ app/src/main/java/com/tomi/ohl/szakdoga/SplashActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.tomi.ohl.szakdoga.controller.FamilyController;
+import com.tomi.ohl.szakdoga.views.FamilyChooserBottomSheet;
 
 import java.util.HashMap;
 
@@ -41,17 +42,23 @@ public class SplashActivity extends AppCompatActivity {
             startActivity(new Intent(this, LoginActivity.class));
             finish();
         } else {
-            // Van, kérjük le a családját, majd irány a főképernyő
+            // Van, kérjük le a családját
             FamilyController.getInstance().getFamily().addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document != null && document.exists()) {
+                        // Van családja, ezt beállítjuk, majd irány a főkepernyő
                         HashMap<String, Object> familydata = (HashMap<String, Object>) document.getData();
                         assert familydata != null;
                         FamilyController.getInstance().setCurrentFamily((String) familydata.get("family"));
                         startActivity(new Intent(SplashActivity.this, MainActivity.class));
                         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                         finish();
+                    } else {
+                        // Kérjük be a családját a bottom sheettel
+                        FamilyChooserBottomSheet familyChooser = new FamilyChooserBottomSheet();
+                        familyChooser.setCancelable(false);
+                        familyChooser.show(getSupportFragmentManager(), "initial_" + FamilyChooserBottomSheet.class.getSimpleName());
                     }
                 }
             });
